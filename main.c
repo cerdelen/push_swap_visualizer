4c0bd5d758255b611f8195cfc274755e5d2ebb81
cerdelen <cedric.erdelen@web.de>
2022-07-15 15:34:35 +0200

    yes

--- main.c
+++ main.c
@@ -22,18 +22,18 @@ bool input_init(int argc, char **argv, t_data *data)
 	return true;
 }
 
-void	free_and_exit(t_data *data)
-{
-	if (data->stacks.stack_a != NULL)
-	{
-		ft_lstclear(&data->stacks.stack_a, ft_lstdelone);
-	}
-	if (data->stacks.stack_b != NULL)
-	{
-		ft_lstclear(&data->stacks.stack_b, ft_lstdelone);
-	}
+// void	free_and_exit(t_data *data)
+// {
+// 	if (data->stacks.stack_a != NULL)
+// 	{
+// 		ft_lstclear(&data->stacks.stack_a, ft_lstdelone);
+// 	}
+// 	if (data->stacks.stack_b != NULL)
+// 	{
+// 		ft_lstclear(&data->stacks.stack_b, ft_lstdelone);
+// 	}
 	
-}
+//}
 
 int main(int argc, char **argv)
 {
@@ -41,11 +41,11 @@ int main(int argc, char **argv)
 	mlx_data	m_data;
 	img_data	i_data;
 
-	if (input_init(argc, argv, &data) == false)
-		free_and_exit(&data);
+	// if (input_init(argc, argv, &data) == false)
+		// free_and_exit(&data);
 
-	printf("%i %i %i %i\n", START_LINE_STACK_A, MIDDLE_LINE_X_VALUE - END_LINE_STACK_A,
-		 START_LINE_STACK_B - MIDDLE_LINE_X_VALUE, WINDOW_W - (END_LINE_STACK_B));
+	// printf("%i %i %i %i\n", START_LINE_STACK_A, MIDDLE_LINE_X_VALUE - END_LINE_STACK_A,
+	// 	 START_LINE_STACK_B - MIDDLE_LINE_X_VALUE, WINDOW_W - (END_LINE_STACK_B));
 	// printf("%f %f %f\n", START_LINE_STACK_A, END_LINE_STACK_A, MIDDLE_LINE_X_VALUE);
 	m_data.mlx = mlx_init();
 	m_data.win = mlx_new_window(m_data.mlx, WINDOW_W, WINDOW_H, "Push-Swap-Visualizer by CErdelen");
@@ -59,7 +59,6 @@ int main(int argc, char **argv)
 	// draw_line_img(&image, 10, START_LINE_STACK_A, 10, END_LINE_STACK_A, 0x00FF0000);
 	// printf("no problem with draw line\n");
 	// mlx_put_image_to_window(m_data.mlx, m_data.win, image.img, 0, 0);
-	printf("%i %i %i %i\n", 10, START_LINE_STACK_A, 10, END_LINE_STACK_A);
 	mlx_loop(m_data.mlx);
 	return (0);
 }
